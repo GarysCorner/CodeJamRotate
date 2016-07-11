736a19e8b95044cc016f671f70fb6c1cd46dcc50
Gary Bezet <gary@USSGW780L.FLUORUSA.LOCAL>
2016-07-11 13:15:35 -0500

    Finished codeing but program results in errors time to troubleshoot

---
+++ codejamrotate/CodeJamRotate.java
@@ -0,0 +1,221 @@
+//File:		CodeJamRotate.java
+//Author:	Gary Bezet
+//Date:		2016-07-11
+//Desc:		Designed to solve google Code Jam Rotate from 2010 Round 1A
+//Problem:	https://code.google.com/codejam/contest/544101/dashboard#s=p0
+//Results:	A-small-practice.in: 69ms       A-large-practice.out: 163ms
+
+package codejamrotate;
+
+import java.io.PrintStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class CodeJamRotate {
+
+    
+    //variables
+    public long starttime;
+    
+    //file streams
+    private BufferedReader infile;
+    private PrintStream outfile;
+    
+    //filename variables
+    private String infileopt, outfileopt;  
+    
+    private long linenum = 0; //keeps track of the current line number of the infile for error printing purposes    
+    
+    private int totalcases;  //total number of cases
+    
+    private TestCase[] testcases;  //holds all the test cases
+    
+       
+    
+    /*
+        Functions
+    */
+    
+    //program starts here
+    public void run(String[] args) {
+        this.loadoptions(args);  //load arguments
+        
+        this.openFiles();
+        
+        this.loadData();
+        
+        //rotate all cases
+        long startrotate = System.currentTimeMillis();
+        for(int i=0; i<totalcases; i++) {
+            
+            testcases[i].rotate();
+            
+        }
+        System.err.printf("%1$d test cases rotated in %2$d...solving\n", totalcases, System.currentTimeMillis() - startrotate );
+        
+        long startsolve = System.currentTimeMillis();
+        for(int i=0; i<totalcases;i++) {
+            testcases[i].solve();
+        }
+        System.err.printf("%1$d test cases solved in %2$dms...\nOutputing data to:  %3$s\n\n", this.totalcases, System.currentTimeMillis()-startsolve, this.outfileopt);
+        
+        long startoutput = System.currentTimeMillis();
+        for(int i=0; i<this.totalcases; i++) {
+            outfile.printf("Case #%1$d: %2$s\n", testcases[i].casenum, testcases[i].answer());
+        }        
+        System.err.printf("%1$d test cases output in %2$dms!\n", this.totalcases, System.currentTimeMillis() - startoutput);
+        
+        this.closeFiles();  //close files
+    }
+    
+    //load data from file into data structure
+    private void loadData() {
+        
+        long loaddatastarttime = System.currentTimeMillis();  //the time we started loading data
+        
+        System.err.printf("\nLoading data from \"%1$s\"...\n", this.infileopt);
+        
+        String line = this.infileReadLine();
+        
+        //get case numbers
+        try{
+            totalcases = Integer.parseInt(line);
+        } catch(NumberFormatException ex) {
+            System.err.printf("Could not parse total number of cases (line #%1$d) from:  %2$s", this.linenum, this.infileopt);
+            System.exit(4);
+        }
+        
+        //initialize testcases array
+        testcases = new TestCase[totalcases];
+        
+        
+        //load all cases
+        for(int i=0; i < this.totalcases; i++) {
+            
+            
+            line = this.infileReadLine(); //get matrix size and k
+
+            String[] matrixandk = line.split(" ");
+            
+            if(matrixandk.length != 2) {  //check formating of matrixandk line and panic
+                System.err.printf("Wrong number of arguments (%3$d) line(%1$d) from:  %2$s", this.linenum, this.infileopt, matrixandk.length);
+                System.exit(5);
+            }
+         
+            //try to parse matrix size and k and initialize the data structure for this test case panic if fail
+            try{
+                testcases[i] = new TestCase(Integer.parseInt(matrixandk[0]), Integer.parseInt(matrixandk[1]), i+1);
+                
+            }catch(NumberFormatException ex) {
+                System.err.printf("Could not parse matrix size and k (line #%1$d) from:  %2$s", this.linenum, this.infileopt);
+                System.exit(5);
+            }    
+            
+            System.err.printf("Reading testcase #%1$d, N=%2$d  K=%3$d\n", this.testcases[i].casenum, this.testcases[i].matrixsize, this.testcases[i].k);
+            
+            //read all rows in matrix
+            for(int c=0; c<testcases[i].matrixsize; c++) {  
+                line = this.infileReadLine();
+               
+                //read all columns in row
+                for(int d=0; d<testcases[i].matrixsize; d++) {
+                    testcases[i].matrix[c][d] = line.charAt(d);
+                }
+                
+                
+            }
+            
+            //System.err.print(this.testcases[i]); //for testing
+            
+        }
+        
+        System.err.printf("All data loaded in %1$dms\n", System.currentTimeMillis() - loaddatastarttime);
+        
+    }
+    
+    //open the files
+    public void openFiles() {
+        try {
+            infile = new BufferedReader(new FileReader(infileopt));
+        } catch (FileNotFoundException ex) {
+            System.err.printf("Error could not open infile \"%1$s\":  $2%s\n", infileopt, ex.toString());
+            System.exit(2);
+        }
+        
+        if(outfileopt == null) {
+            outfileopt = "Stdout";
+            outfile = System.out;
+        } else {
+            try {
+                outfile = new PrintStream(new File(outfileopt));
+            } catch (FileNotFoundException ex) {
+                System.err.printf("Error could not open outfile \"%1$s\":  $2%s\n", outfileopt, ex.toString());
+                System.exit(2);
+            }
+        }
+        
+        System.err.printf("Infile:  %1$s\nOutfile:  %2$s\n", infileopt, outfileopt);
+        
+        
+                
+    }
+    
+    //reads a line panic if exception
+    private String infileReadLine() {  
+        
+        linenum++;
+
+        try {
+                return infile.readLine();
+        } catch (IOException e) {
+                System.err.printf("Unable to read line #%3$d from \"%1$s\":  %2$s\n", infileopt, e.toString(), this.linenum);
+                System.exit(3);
+        }
+
+        return null;  //get rid of netbeans error   
+    }
+    
+    //close files
+    private void closeFiles() {
+        try {
+            infile.close();
+        } catch (Exception ex) {
+            //do nothing the program is already finished
+        }
+        
+        outfile.close();
+    }
+    
+    //loads the command line options
+    private void loadoptions(String[] args) {
+        
+		
+        if(  2 < args.length || args.length == 0 ) {
+                System.err.println("Program requires 1 or 2 arguments.  First arg is infile name, 2nd arg is outfile name StdOut by default.");
+                System.exit(1);  //exit the system if arguments not correct
+        }
+
+        infileopt = args[0];
+
+        if( args.length == 2 ) {  //set the outfile if exists
+                outfileopt = args[1];
+        }
+
+    }
+    
+    public static void main(String[] args) {
+        //load class and call run()
+        CodeJamRotate prog = new CodeJamRotate();
+        prog.starttime = System.currentTimeMillis();
+        prog.run(args);
+        System.err.printf("Program finished in %1$dms\n", System.currentTimeMillis() - prog.starttime);
+        
+    }
+    
+}
---
+++ codejamrotate/TestCase.java
@@ -0,0 +1,203 @@
+//File:		TestCase.java
+//Author:	Gary Bezet
+//Date:		2016-07-11
+//Desc:		Designed to solve google Code Jam Rotate from 2010 Round 1A
+//Problem:	https://code.google.com/codejam/contest/544101/dashboard#s=p0
+//Results:	
+package codejamrotate;
+
+
+//Data structure for a single testcase
+public class TestCase {
+    
+    //variables
+    public int matrixsize;  //size of matrix
+    public int k;  //the number in a row you need
+    
+    public int casenum; //case number
+    
+    public char[][] matrix;  //the data structure
+    
+    //keep track of who has k in a row so far
+    boolean rhask = false;
+    boolean bhask = false;
+
+    //how many have we currently found when one of these hits k we have a winner
+    int rinline = 0;  
+    int binline = 0;
+    
+    //initialize data structure
+    TestCase(int matrixsize, int k, int casenum) {
+        this.matrixsize = matrixsize;
+
+        this.k = k;
+        
+        this.casenum = casenum;
+        
+        this.matrix = new char[matrixsize][matrixsize]; //make the matrix
+     
+        
+        
+    }
+    
+    
+    //solve this case
+    public void solve() {
+                   
+        
+        //check for accross solution
+        //iter rows
+        
+        for(int r=0; r<this.matrixsize; r++) {
+            //iter columns
+            for(int c=0; c<this.matrixsize; c++) {
+                this.randblinelogic(r, c);
+            }
+            this.rinline = 0;
+            this.binline = 0;
+        }
+        
+        //check if 4 in a row in column
+        //iter over column
+        for(int c=0; c<this.matrixsize; c++) {
+            //iter over rows
+            for(int r=0; r<this.matrixsize; r++) {
+                this.randblinelogic(r, c);
+            }
+        }
+        
+        //solve diagnoal top left to bottom right
+        //iter over rows
+        for(int r=0; r<this.matrixsize; r++) {
+            //iter over columns
+            for(int c=0; c<this.matrixsize; c++) {
+                //move
+                for(int offset = 0; offset + c < this.matrixsize && offset + r < this.matrixsize; offset++ ) {
+                    this.randblinelogic(r+offset, c+offset);//check the logic
+                }
+                rinline = 0;  //reset r and b
+                binline = 0;
+            }
+        }
+        
+        //solve diagnoal top right to bottom left
+        //iter over rows
+        
+        for(int r=0; r<this.matrixsize; r++) {
+            //iter over columns
+            for(int c=0; c<this.matrixsize; c++) {
+                //move
+                
+                for(int offset = 0; c-offset >= 0 && offset + r < this.matrixsize; offset++  ) {
+                    this.randblinelogic(r+offset, c-offset);
+                }
+                
+                rinline=0;  //reset r and b line counts
+                binline=0;
+            }
+            
+        }
+        
+    }
+    
+    //get the answer back as the expected string
+    public String answer () {
+        if( rhask && bhask ) {
+            return "Both";
+        } else if(rhask) {
+            return "Red";
+        } else if(bhask) {
+            return "Blue";
+        } else {
+            return "Neither";
+        }
+        
+    }
+    
+    private void randblinelogic(int r, int c) {
+        switch (this.matrix[r][c]) {
+            case 'R':
+                //R has this spot
+                rinline++; //increment r
+                binline=0; //reset b
+                break;
+            case 'B':
+                //B has this spot
+                binline++;  //increment b reset r
+                rinline=0;
+                break;
+            default:
+                //reset r and b
+                rinline=0;
+                binline=0;
+                break;
+        }
+
+        if(rinline >= this.k) {
+            rhask = true;
+        }
+        if(binline >= this.k) {
+            bhask = true;
+        }
+    }
+    
+    //rotate the data structure
+    public void rotate() {
+        char[][] newmatrix = new char[this.matrixsize][this.matrixsize];
+        
+        //this code rotates then we will use gravity
+        //iter over rows
+        for(int r=0; r<this.matrixsize; r++) {
+            
+            //iter over columns
+            for(int c=0; c<this.matrixsize; c++) {
+                newmatrix[c][this.matrixsize-r-1] = this.matrix[r][c];
+            }
+        }
+        
+        //set the matrix to the new rotated matrix
+        this.matrix = newmatrix;
+        
+        //gravity effect
+        
+        //iter throw rows backwards ignore row 1 because it is own bottom
+        for(int r=this.matrixsize-2; r>=0; r--) {
+            //iter through colums order shouldn't matter
+            for(int c=0; c<this.matrixsize; c++) {
+                
+                if(this.matrix[r][c] == '.') {
+                    continue;
+                }
+                
+                int rowdrop = 0; //how many rows to drop
+                for(int lowspot=r+1; (lowspot)<this.matrixsize && this.matrix[lowspot][c] == '.'  ; lowspot++) {
+                    rowdrop++;
+                }
+                if(rowdrop > 0) {
+                    this.matrix[r+rowdrop][c] = this.matrix[r][c];
+                    this.matrix[r][c] = '.';
+                }
+                    
+            }
+        }
+        
+    }
+    
+    //stringify  we will probably only use this for testing but good to have
+    @Override public String toString() {
+        
+        StringBuilder result = new StringBuilder();
+        
+        //iterate over rows
+        for(int r=0; r<this.matrixsize; r++) {
+            //iterate over columns
+            for(int c=0; c< this.matrixsize; c++) {
+                result.append(this.matrix[r][c]);
+            }
+            result.append(System.lineSeparator());
+        }
+        
+        return result.toString();
+    }
+    
+}
